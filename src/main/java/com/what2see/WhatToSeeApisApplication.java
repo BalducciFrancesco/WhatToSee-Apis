5018233014898124cd4b681c996a5793ed8d3416
Francesco Balducci <francesco.balducci@edu.elis.org>
2023-03-29 15:42:09 +0200

    Initial commit

---
+++ src/main/java/com/what2see/WhatToSeeApisApplication.java
@@ -0,0 +1,13 @@
+package com.what2see;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToSeeApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatToSeeApisApplication.class, args);
+    }
+
+}
